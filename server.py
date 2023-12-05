922ac165e065d72f63fb2fff908884f578ee4f4f
Victoria Vu <victoriamvvu@gmail.com>
2023-12-05 14:21:24 -0800

    Implement route to view details of a particular movie and create movie details template page

--- server.py
+++ server.py
@@ -26,6 +26,16 @@ def all_movies():
 
     return render_template("all_movies.html", movies=movies)
 
+
+@app.route("/movies/<movie_id>")
+def show_movie(movie_id):
+    """Show details about a particular movie."""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template("movie_details.html", movie=movie)
+
+
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
