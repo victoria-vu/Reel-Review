6b6bff772b31e25ccd0fd0044902de99ddcdc962
Victoria Vu <victoriamvvu@gmail.com>
2023-12-05 13:38:54 -0800

    Import Flask dependencies, StrictUndefined from jinja2, crud

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 """Server for Reel Reviews app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session, redirect)
 from model import connect_to_db
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 app.app_context().push()
+
+
+@app.route("/")
+def homepage():
+    """View homepage."""
+
+    return render_template("homepage.html")
 
 
 if __name__ == "__main__":
