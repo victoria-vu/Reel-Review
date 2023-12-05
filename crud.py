4157d02ea1ddbc9a0d41f6527311fb302358e433
Victoria Vu <victoriamvvu@gmail.com>
2023-12-05 14:19:50 -0800

    Create function to retrieve a movie by movie id

--- crud.py
+++ crud.py
@@ -50,6 +50,12 @@ def get_movies():
     return Movie.query.all()
 
 
+def get_movie_by_id(movie_id):
+    """Return a movie by movie id."""
+
+    return Movie.query.get(movie_id)
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
