50e626801b3059723db8b9f0d55382d17a3d2746
Victoria Vu <victoriamvvu@gmail.com>
2023-12-05 19:51:41 -0800

    Create functions to get all users from database and get user by user id

--- crud.py
+++ crud.py
@@ -56,6 +56,18 @@ def get_movie_by_id(movie_id):
     return Movie.query.get(movie_id)
 
 
+def get_users():
+    """Return all users from the database."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return a user by user id."""
+
+    return User.query.get(user_id)
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
