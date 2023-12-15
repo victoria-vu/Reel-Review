875aacbc8efd71f4057ad8adabd79c33857a977d
Victoria Vu <victoriamvvu@gmail.com>
2023-12-15 11:28:24 -0800

    Create routes to sign up, view login page, login, and logout

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for Reel Reviews app."""
 
 from flask import (Flask, render_template, request, flash, session, redirect)
-from model import connect_to_db
+from model import connect_to_db, db
 import crud
 from jinja2 import StrictUndefined
 
@@ -52,6 +52,70 @@ def show_user(user_id):
     user = crud.get_user_by_id(user_id)
 
     return render_template("user_details.html", user=user)
+
+
+@app.route("/signup", methods=["POST"])
+def signup_user():
+    """Create a new user."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash("A user with that email address already exists. Please try a different email address to register, or log in to your existing account.")
+    else:
+        user = crud.create_user(email, password, fname, lname)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been successfully created! Please log in.")
+
+    return redirect("/login")
+
+
+@app.route("/login")
+def login_page():
+    """View log in page."""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    """Log in an existing user."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    if not user:
+        flash("The email you typed in does not exist. Please sign up for an account or try again.")
+    elif user:
+        if password == user.password:
+            session["user_id"] = user.user_id
+            session["name"]= user.fname
+            session["email"] = user.email
+            flash("Logged in successfully.")
+            return redirect("/")
+        else:
+            flash("Incorrect password. Please try again.")
+
+    return redirect("/login")
+
+
+@app.route("/logout")
+def logout_user():
+    """Log a user out of the session."""
+
+    if "user_id" in session:
+        session.clear()
+        flash("You have signed out.")
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
