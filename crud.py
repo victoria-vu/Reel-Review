188735d1c18be874688751514ce83eaed72f4edd
Victoria Vu <victoriamvvu@gmail.com>
2023-12-15 11:27:50 -0800

    Create function to retrieve user by email

--- crud.py
+++ crud.py
@@ -68,6 +68,12 @@ def get_user_by_id(user_id):
     return User.query.get(user_id)
 
 
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
